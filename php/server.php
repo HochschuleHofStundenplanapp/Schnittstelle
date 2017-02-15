74a5e6c9bc8f2037c91ce8e065ca3573f26d2654
Daniel Glaser <danielglaser9@gmail.com>
2017-02-15 13:35:48 +0100

    Server.php auch mit Backup abgeglichen.

--- php/server.php
+++ php/server.php
@@ -22,7 +22,8 @@ const __VERSIONNUMBER = 3.2;
 
 $server = new SoapServer(
         null, array(                    //Parameter	Bedeutung                       Priorität
-    'uri' => "http://localhost/soap",   //uri           Namespace des SOAP-Service	notwendig wenn WSDL nicht genutzt wird            
+##    'uri' => "http://localhost/soap",   //uri           Namespace des SOAP-Service	notwendig wenn WSDL nicht genutzt wird            
+    'uri' => "https://app.hof-university.de/soap",   //uri           Namespace des SOAP-Service	notwendig wenn WSDL nicht genutzt wird            
     'encoding' => 'UTF-8',              //encoding	Zeichensatz für SOAP-Request	optional
     'soap_version' => SOAP_1_2          //soap_version	Eingesetzte Protokollversion	optional
         )
