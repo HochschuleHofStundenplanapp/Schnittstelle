2130fa4072ca258496774f4bf3ba2314b2e15260
stepping <michael.stepping@hof-university.de>
2017-02-15 15:08:28 +0100

    Connection credentials updated

--- php/connect_db.php
+++ php/connect_db.php
@@ -7,13 +7,17 @@
 // Unter Windows ist das Auflösen von localhost seit Vista sehr langsam, es wird wirklich eine DNS Abfrage getätigt 
 //$mysql_host = 'localhost';
 $mysql_host = '127.0.0.1';
-$mysql_db = 'test';
-$mysql_user = '';
-$mysql_password = '';
+$mysql_db = "t3_ext";
+$mysql_user = "appuser";
+$mysql_password = "Wer8%GreitY99#";
 
 
 try {
     $pdo = new PDO("mysql:host=$mysql_host;dbname=$mysql_db;charset=utf8", $mysql_user, $mysql_password);
+
+    //print "Success";
+    // #    $pdo->exec("set names utf8");	// nur bei PHP < 5.4
+
 } catch (PDOException $e) {
     print 'Error!: ' . $e->getMessage() . '<br />';
     die();
