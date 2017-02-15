a3f0b64362b265c0edbfa3c147832a29377edd9a
stepping <michael.stepping@hof-university.de>
2017-02-15 16:43:54 +0100

    Bug fix: Unix will nur "localhost" akzeptieren

--- php/connect_db.php
+++ php/connect_db.php
@@ -4,18 +4,20 @@
  */
  
 
-// Unter Windows ist das Auflösen von localhost seit Vista sehr langsam, es wird wirklich eine DNS Abfrage getätigt 
-//$mysql_host = 'localhost';
-$mysql_host = '127.0.0.1';
+/* Unter Windows ist das Auflösen von localhost seit Vista sehr langsam, es wird wirklich eine DNS Abfrage getätigt 
+	
+	Achtung: MS 20170215: Unter dem App Server müssen wir "localhost" verwenden. Bei 127.0.0.1 erhalten
+	wir eine Fehlermeldung in der JSON-Verarbeitung...
+*/
+/* $mysql_host = '127.0.0.1'; unter Windows */
+$mysql_host = "localhost";
 $mysql_db = "t3_ext";
 $mysql_user = "appuser";
 $mysql_password = "Wer8%GreitY99#";
 
-
 try {
     $pdo = new PDO("mysql:host=$mysql_host;dbname=$mysql_db;charset=utf8", $mysql_user, $mysql_password);
 
-    //print "Success";
     // #    $pdo->exec("set names utf8");	// nur bei PHP < 5.4
 
 } catch (PDOException $e) {
