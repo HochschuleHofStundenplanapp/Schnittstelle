97f276b03d4f1cd8415e0ca131630d6d420d3c26
Daniel Glaser <danielglaser9@gmail.com>
2017-02-21 16:53:11 +0100

    FCM: Typ text zu varchar gemacht und Indizes wieder eingefügt.

--- MySQL-Server/CREATE tables for FCM.sql
+++ MySQL-Server/CREATE tables for FCM.sql
@@ -5,8 +5,15 @@
 CREATE TABLE `fcm_nutzer` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `vorlesung_id` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `vorlesung_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indizes für die Tabelle `fcm_nutzer`
+--
+ALTER TABLE `fcm_nutzer`
+  ADD INDEX `token` (`token`),
+  ADD INDEX `vorlesung_id` (`vorlesung_id`);
 
 
 --
@@ -15,6 +22,13 @@ CREATE TABLE `fcm_nutzer` (
 
 CREATE TABLE `fcm_verlegungen` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `verlegung_id` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `vorlesung_id` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `verlegung_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `vorlesung_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indizes für die Tabelle `fcm_verlegungen`
+--
+ALTER TABLE `fcm_verlegungen`
+  ADD INDEX `verlegung_id` (`verlegung_id`),
+  ADD INDEX `vorlesung_id` (`vorlesung_id`);
